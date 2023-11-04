d748ff2f8d572d4c66f27f9a19f2ed3a37d3fc41
mallikarjuna <malli@gmail.com>
2023-11-04 15:57:31 +0000

    adding 2 files

---
+++ charan1.java
@@ -0,0 +1,6 @@
+public class charan1{
+public static void main(String[] args)
+	{
+		System.out.println("Hello Jenkins, from charan1 file");
+	}
+}
---
+++ charan2.java
@@ -0,0 +1,6 @@
+public class charan2{
+public static void main(String[] args)
+        {
+                System.out.println("Hello Jenkins, from charan2 file");
+        }
+}

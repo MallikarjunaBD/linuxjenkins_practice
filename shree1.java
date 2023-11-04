c9277e1f910742d8a48f8ebdd969ca076dee4fb2
mallikarjuna <malli@gmail.com>
2023-11-04 15:11:41 +0000

    deleting 2 file

--- shree1.java
+++
@@ -1,6 +0,0 @@
-public class shree1{
-public static void main(String[] args)
-        {
-                System.out.println("Hello Jenkins, from shree1 file");
-        }
-}
--- shree2.java
+++
@@ -1,6 +0,0 @@
-public class shree2{
-public static void main(String[] args)
-        {
-                System.out.println("Hello Jenkins, from shree2 file");
-        }
-}

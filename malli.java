3a455765347f0728ebe973963160bf92e74356f6
malli <malli@gmail.com>
2023-11-04 15:37:39 +0000

    adding 2file from malli

---
+++ malli.java
@@ -0,0 +1,6 @@
+public class malli1{
+public static void main(String[] args)
+	{
+	System.out.println("Hello Jenkins from malli1 file")
+	}
+}
---
+++ malli2.java
@@ -0,0 +1,6 @@
+public class malli2{
+public static void main(String[] args)
+        {
+                System.out.println("Hello Jenkins, from malli2 file");
+        }
+}
